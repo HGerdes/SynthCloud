71eef888e80d3b8f0f5e183019a1afee5100ad49
Harrison Gerdes <harrisongerdes@gmail.com>
2021-11-28 00:01:17 -0500

    modal works

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect, NavLink } from "react-router-dom";
 import { login } from "../../store/session";
+import LoginFormModal from "../LoginFormModal";
 import "./splash.css"
 
 
@@ -33,8 +34,8 @@ const Splash = () => {
                         <div className="splashSignUp">
                             <NavLink to="/signup" className="splashSignUpLink splashBtn">Sign Up</NavLink>
                         </div>
-                        <div className="splashLogin">
-                            <NavLink to="/login" className="splashLoginLink splashBtn">Log In</NavLink>
+                        <div className="splashLogin" >
+                                <LoginFormModal />
                         </div>
                         <div className="splashDemoBtn">
                             <button onClick={demo} className="splashDemoBtn splashBtn">Demo User</button>
