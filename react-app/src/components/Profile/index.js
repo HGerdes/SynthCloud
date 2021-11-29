928306f84a1c6f07d0f564562d7c0cd5c270f1f4
Harrison Gerdes <harrisongerdes@gmail.com>
2021-11-29 12:30:59 -0500

    profile page done

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -17,17 +17,19 @@ const Profile = () => {
     return (
         <>
             <div className="profilePageContainer">
-                <div className="trackListContainer">
+                <div className="profilePageInnerStuff">
+                    <div className="profileTitle">Your Uploads</div>
+                    <div className="commenthr" id="profilehr"></div>
                         {tracks?.map(track => (
-                        <div className="trackContainer" key={`${track.track?.id}`}>
+                        <div key={`${track.track?.id}`}>
                             {track.track?.user_id === userId ?
-                             <NavLink to={`/tracks/${track.track?.id}`}>
+                             <NavLink to={`/tracks/${track.track?.id}`} className="trackLink">
                                 <div className="profileTrackname"> {track.track?.name}
                                     <img id="profileTrackImages" src={track.track?.image_url}>
                                     </img>
                                 </div>
                                 </NavLink>
-                            : null}
+                            : ""}
                         </div>
                     ))}
                     <div id="profileImgSrc"></div>
